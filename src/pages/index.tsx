fb74c5037a69d5386ae872e8a1b48547ad4f5119
Raul Sanz <estraulsanz@gmail.com>
2021-08-14 02:44:01 -0500

    create template

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import * as React from "react"
+
+// markup
+const IndexPage = () => {
+  return (
+    <main >
+      <p className="text-red-700">hola</p>
+    </main>
+  )
+}
+
+export default IndexPage
