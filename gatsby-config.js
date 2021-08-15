49d753ae45b6235fb0803b3f09c3734c8f3a8776
Raul Sanz <estraulsanz@gmail.com>
2021-08-14 23:56:31 -0500

    Add config for Graphql plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,35 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
   siteMetadata: {
     siteUrl: "https://www.yourdomain.tld",
     title: "My Gatsby Site",
   },
-  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-postcss"],
+  plugins: [
+    "gatsby-plugin-styled-components", 
+    "gatsby-plugin-postcss",
+    {
+      resolve: "gatsby-source-graphql",
+      options: {
+        typeName: "GitHub",
+        fieldName: "github",
+        url: "https://api.github.com/graphql",
+        // HTTP headers
+        headers: {
+          // Learn about environment variables: https://gatsby.dev/env-vars
+          Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
+        },
+        // HTTP headers alternatively accepts a function (allows async)
+        headers: async () => {
+          return {
+            Authorization: await getAuthorizationToken(),
+          }
+        },
+        // Additional options to pass to node-fetch
+        fetchOptions: {},
+      },
+    },
+  ],
 };
